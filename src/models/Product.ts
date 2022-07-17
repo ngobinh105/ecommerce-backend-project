1fa7b54715ae16c194b113dc06221a66845b4341
Gajalakshan C <gajalakshan24@yahoo.com>
2022-07-17 12:25:25 +0300

    add mongo shema model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,10 +5,11 @@ const Users_infoSchema = new Schema(
         title: {type: String, required: true, maxlength: 60},
         description: {type: String,  maxlength: 200},
         discount: {type: Number,  min:0, max: 100},
+        price: {type: Number, min:0, max: 1000},
         quantity: {type: Number, min:0, max: 1000},
         categoryId: {type: Number, min:1, required: true},
-        images: {type: String[], maxlength: 60, required: true},
-       // status: {type: Number, required: true}
+        images:[{type: String, maxlength: 100, required: true}],
+       // status: {type: Number, required: true} if we suspend the product 
     }
 );
 //Export Product model , products will be appier in mogodb
