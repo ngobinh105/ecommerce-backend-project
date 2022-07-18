5f3f5fd88ccbba56476d6d85e7755812bdacb2c4
Gajalakshan C <gajalakshan24@yahoo.com>
2022-07-18 22:44:26 +0300

    category objectId still has problem

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = 8080
 
 console.log(process.env.DB_URL)
 mongoose
-  .connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.1')
+  .connect(process.env.DB_URL || 'mongodb://127.0.0.1:27017/test?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.1')
   .then(() => {
     app.listen(port, () => console.log(`Server is running on port: ${port}`))
   })
@@ -15,3 +15,5 @@ mongoose
     console.log('failed connection to DB')
     process.exit(1)
   })
+
+   
