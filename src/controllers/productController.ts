e278681589fd6453a48eafb08d0744054181a794
Gajalakshan C <gajalakshan24@yahoo.com>
2022-07-18 22:35:45 +0300

    remove console.log

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,27 +1,23 @@
 import { Request, Response } from "express"
 import productService from "../services/productService"
 const getAllProducts = async (req :Request, res: Response)=>{
-    console.log("getAllProducts");
     const products = await productService.getAllProduct()
     return res.json(products)
 }
 
 const getSingleProduct = async (req :Request, res: Response)=>{
-    console.log("getSingleProduct");
     const { productId } = req.params
     const product = await productService.getSingleProduct(productId)
     return res.json(product)
 }
 
 const createProduct = async (req :Request, res: Response)=>{
-    console.log("createProduct");
     const { product } = req.body
     const productCreate = await productService.insertProduct(product)
     return res.json(productCreate)
 }
 
 const updateProduct = async (req :Request, res: Response)=>{
-    console.log("updateProduct");
     const { productId } = req.params
     const { product } = req.body
     const productUpdate = await productService.updateProduct(productId, product)
@@ -29,7 +25,6 @@ const updateProduct = async (req :Request, res: Response)=>{
 }
 
 const deleteProduct = async (req :Request, res: Response)=>{
-    console.log("deleteProduct");
     const { productId } = req.params
     const product = await productService.deleteProduct(productId)
     return res.json(product)
