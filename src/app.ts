a937674af3559014c7616ad278a46eb5b5845e1c
Zeriab Chiah <zeriab@hotmail.com>
2022-07-18 17:05:22 +0300

    push

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import path from 'path'
+import YAML from 'yamljs'
+import swaggerUi from 'swagger-ui-express'
+
+const swaggerDocument = YAML.load(
+  path.join(__dirname, '../_build/swagger.yaml')
+)
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(express.text())
+
+// route
+
+//swagger api docs
+app.use(
+  '/',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    explorer: true,
+  })
+)
+
+export default app
