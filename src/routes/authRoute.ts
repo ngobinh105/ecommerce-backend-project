47699d6891405d443a545448ff96cd370846f6a8
Binh Ngo <binhngo1005@gmail.com>
2022-07-19 18:00:50 +0300

    add passport & google strategy

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import passport from 'passport'
 
 import userController from '../controllers/userController'
 import { verifyUserLogin } from '../middlewares/userMiddlewares'
@@ -6,5 +7,10 @@ import { verifyUserLogin } from '../middlewares/userMiddlewares'
 const authRoute = Router()
 authRoute.post('/login', verifyUserLogin, userController.userLogin)
 authRoute.post('/profile', userController.verifyUserToken)
+authRoute.post(
+  '/googleLogin',
+  passport.authenticate('google', { scope: ['profile'] }),
+  userController.userLogin
+)
 
 export default authRoute
